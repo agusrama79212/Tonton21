9e4aec0237e268b5fd66339891abf5cb2fa60bdc
AgungSudi <igustiagungsudiadnyana@gmail.com>
2025-12-11 09:28:33 -0800

    Add DirectLink

--- src/pages/VerifLink.tsx
+++ src/pages/VerifLink.tsx
@@ -13,7 +13,7 @@ export const VerifLink: React.FC = () => {
   const [isProcessing, setIsProcessing] = useState(false);
 
   const adUrls = [
-    'https://agungwandev.com',
+    'https://otieu.com/4/10069080',
   ];
 
   useEffect(() => {
